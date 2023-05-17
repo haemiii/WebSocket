144bf59475d78cdfa6e1424fed6f00724e6169d5
haemiii <73984822+haemiii@users.noreply.github.com>
2023-05-17 12:21:08 +0900

    socket.io

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,24 +1,25 @@
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const socket = io(); //socket.io를 쓰고있는 서버를 자동적으로 찾는다!
 
-// front에서 backend로 연결
-const socket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
 
-socket.addEventListener("open", () => {
-  console.log("Connected to Server ✅");
-});
+room.hidden = true;
 
-socket.addEventListener("message", (message) => {
-  console.log("New message", message.data, "from the server");
-});
+let roomName;
 
-socket.addEventListener("close", () => {
-  console.log("Connected to Server ❌");
-});
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(input.value);
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
 }
-messageForm.addEventListener("submit", handleSubmit);
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, showRoom); // -> event, args
+  roomName = input.value;
+  input.value = "";
+}
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-import WebSocket, { WebSocketServer } from "ws";
+// import WebSocket from "ws";
+import SocketIo from "socket.io";
 import http from "http";
 
 const app = express();
@@ -15,18 +16,34 @@ console.log("hello");
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app); //http 서버
-const wss = new WebSocket.Server({ server }); //webSocket 서버
+const io = SocketIo(server); //socketio 서버
 
-const sockets = [];
-wss.on("connection", (socket) => {
-  console.log(socket);
-  //백엔드와 연결한 각 브라우저를 위한 것
-  sockets.push(socket);
-  console.log("Connected to Browser ✅");
-  socket.on("close", () => console.log("Disconnected from the Browser"));
-  socket.on("message", (message) => {
-    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+io.on("connection", (socket) => {
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done(); // callback function(front에서 보낸 함수!)
   });
 });
+// const wss = new WebSocket.Server({ server }); //webSocket 서버
+
+// const sockets = [];
+// wss.on("connection", (socket) => {
+//   //백엔드와 연결한 각 브라우저를 위한 것
+//   sockets.push(socket);
+//   console.log("Connected to Browser ✅");
+//   socket.on("close", () => console.log("Disconnected from the Browser"));
+//   socket["nickname"] = "Anon";
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}: ${message.payload}`)
+//         );
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//     }
+//   });
+// });
 
 server.listen(3000, handleListen);
